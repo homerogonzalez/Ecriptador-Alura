8fc795091bf18e8b6ce9ba0f5fbbbc1fcf7f592e
homerogonzalez <fernandohomeroga@gmail.com>
2024-08-24 21:12:03 -0700

    update index.html, style.css

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
+let texto = document.getElementById('mensaje').value;
+
 function encriptar() {
     let texto = document.getElementById('mensaje').value;
+    if (/[^a-z\s]/.test(texto)) {
+        alert("Solo minúsculas y sin acentos");
+        return;
+      }
     //onsole.log(texto);
     texto = texto.replaceAll("e", "enter").replaceAll("i", "imes").replaceAll("a","ai").replaceAll("o","ober").replaceAll("u","ufat");   
+    // document.getElementById('mensaje2').style.backgroundImage = none;
     document.getElementById('mensaje2').innerHTML = texto;
     return;
 }
 
 function desencriptar() {
+    if (/[^a-z\s]/.test(texto)) {
+        alert("Solo minúsculas y sin acentos");
+        return;
+      }
     let texto = document.getElementById('mensaje').value;
     texto = texto.replaceAll("enter", "e").replaceAll("imes", "i").replaceAll("ai","a").replaceAll("ober","o").replaceAll("ufat","u");   
     document.getElementById('mensaje2').innerHTML = texto;
